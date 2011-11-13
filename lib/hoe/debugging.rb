1526cb9ff08c8ef7cd2314257ff913054354d58d
Mike Dalessio <mike@csa.net>
2011-11-13 12:46:43 -0500

    Fixing reference to ::RUBY with rbconfig-based construction of the ruby interpreter path.

--- lib/hoe/debugging.rb
+++ lib/hoe/debugging.rb
@@ -1,3 +1,5 @@
+require 'rbconfig'
+
 class Hoe #:nodoc:
 
   # Whee, stuff to help when your codes are b0rked. Tasks provided:
@@ -31,30 +33,35 @@ class Hoe #:nodoc:
                                "--undef-value-errors=no"]
     end
 
+    def ruby
+      # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/151376
+      @ruby ||= File.join(Config::CONFIG["bindir"], (Config::CONFIG["RUBY_INSTALL_NAME"] + Config::CONFIG["EXEEXT"]))
+    end
+
     def define_debugging_tasks #:nodoc:
       if File.directory? "test" then
         desc "Run the test suite under GDB."
         task "test:gdb" do
-          sh "gdb #{gdb_options.join ' '} --args #{RUBY} #{make_test_cmd}"
+          sh "gdb #{gdb_options.join ' '} --args #{ruby} #{make_test_cmd}"
         end
 
         desc "Run the test suite under Valgrind."
         task "test:valgrind" do
-          sh "valgrind #{valgrind_options.join ' '} #{RUBY} #{make_test_cmd}"
+          sh "valgrind #{valgrind_options.join ' '} #{ruby} #{make_test_cmd}"
         end
 
         desc "Run the test suite under Valgrind with memory-fill."
         task "test:valgrind:mem" do
           sh "valgrind #{valgrind_options.join ' '} " +
              "--freelist-vol=100000000 --malloc-fill=6D --free-fill=66 " +
-             "#{RUBY} #{make_test_cmd}"
+             "#{ruby} #{make_test_cmd}"
         end
 
         desc "Run the test suite under Valgrind with memory-zero."
         task "test:valgrind:mem0" do
           sh "valgrind #{valgrind_options.join ' '} " +
              "--freelist-vol=100000000 --malloc-fill=00 --free-fill=00 " +
-             "#{RUBY} #{make_test_cmd}"
+             "#{ruby} #{make_test_cmd}"
         end
       end
     end
