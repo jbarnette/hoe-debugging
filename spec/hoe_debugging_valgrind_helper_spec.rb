574b1e7c905f417defa572cfa7ed8bee125bbef8
Mike Dalessio <mike.dalessio@gmail.com>
2017-01-20 21:03:00 -0500

    upgrade to modern rspec usage

--- spec/hoe_debugging_valgrind_helper_spec.rb
+++ spec/hoe_debugging_valgrind_helper_spec.rb
@@ -10,24 +10,24 @@ describe Hoe::Debugging::ValgrindHelper do
 
   describe "#initialize" do
     it "requires a project_name argument" do
-      proc { klass.new }.should raise_error ArgumentError
+      expect { klass.new }.to raise_error ArgumentError
     end
   end
 
   describe "#directory" do
-    it { klass.new("myproj")                     .directory.should == "suppressions" }
-    it { klass.new("myproj", :directory => "foo").directory.should == "foo" }
+    it { expect(klass.new("myproj")                     .directory).to eq "suppressions" }
+    it { expect(klass.new("myproj", :directory => "foo").directory).to eq "foo" }
   end
 
   describe "#project_name" do
-    it { klass.new("myproj").project_name.should == "myproj" }
+    it { expect(klass.new("myproj").project_name).to eq "myproj" }
   end
 
   describe "#version_matches" do
     it "returns possible suppression file matches in order of specificity" do
       helper = klass.new "myproj"
-      helper.stub(:formatted_ruby_version) { "ruby-1.9.3.194" }
-      helper.version_matches.should == %w[ruby-1.9.3.194 ruby-1.9.3 ruby-1.9]
+      allow(helper).to receive(:formatted_ruby_version) { "ruby-1.9.3.194" }
+      expect(helper.version_matches).to eq %w[ruby-1.9.3.194 ruby-1.9.3 ruby-1.9]
     end
   end
 
@@ -38,7 +38,7 @@ describe Hoe::Debugging::ValgrindHelper do
 
       expected = File.read suppfile
 
-      suppressions.should == expected
+      expect(suppressions).to eq expected
     end
   end
 
@@ -46,11 +46,11 @@ describe Hoe::Debugging::ValgrindHelper do
     it "saves a file simply containing the ruby" do
       helper = klass.new "myproj", :directory => suppressions_directory
       actual_file = helper.save_suppressions_from logfile
-      actual_file.should =~ /myproj_ruby-.*\.supp/
+      expect(actual_file).to match(/myproj_ruby-.*\.supp/)
 
       actual = File.read actual_file
       expected = File.read suppfile
-      actual.should == expected
+      expect(actual).to eq expected
     end
   end
 
@@ -63,7 +63,7 @@ describe Hoe::Debugging::ValgrindHelper do
         FileUtils.mkdir_p suppressions_directory
         FileUtils.touch inexact_match
         FileUtils.touch exact_match
-        helper.matching_suppression_file.should == exact_match
+        expect(helper.matching_suppression_file).to eq exact_match
       end
     end
 
@@ -74,7 +74,7 @@ describe Hoe::Debugging::ValgrindHelper do
         inexact_match = File.join(suppressions_directory, "myproj_#{helper.version_matches[1]}.999999999.supp")
         FileUtils.mkdir_p suppressions_directory
         FileUtils.touch inexact_match
-        helper.matching_suppression_file.should == inexact_match
+        expect(helper.matching_suppression_file).to eq inexact_match
       end
     end
 
@@ -84,14 +84,14 @@ describe Hoe::Debugging::ValgrindHelper do
         exact_match = File.join(suppressions_directory, "myproj_#{helper.formatted_ruby_version}.supp")
         FileUtils.mkdir_p suppressions_directory
         FileUtils.touch exact_match
-        helper.matching_suppression_file.should == exact_match
+        expect(helper.matching_suppression_file).to eq exact_match
       end
     end
 
     context "there are zero matches" do
       it "returns nil" do
         helper = klass.new "myproj", :directory => suppressions_directory
-        helper.matching_suppression_file.should == nil
+        expect(helper.matching_suppression_file).to be_nil
       end
     end
   end
