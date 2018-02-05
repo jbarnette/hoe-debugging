a702bb4596489b5e82f3165263d431f65c3ec0a8
Mike Dalessio <mike.dalessio@gmail.com>
2018-02-04 22:32:49 -0500

    finally fix the bundler issue

--- spec/hoe_debugging_exit_code_spec.rb
+++ spec/hoe_debugging_exit_code_spec.rb
@@ -5,18 +5,30 @@ describe "exit code" do
     Rake.sh command, verbose: false
   end
 
+  def do_in_test_dir
+    Bundler.with_clean_env do
+      Dir.chdir test_dir do
+        saved_gemfile = ENV['BUNDLE_GEMFILE']
+        ENV['BUNDLE_GEMFILE'] = "./Gemfile"
+        begin
+          yield
+        ensure
+          ENV['BUNDLE_GEMFILE'] = saved_gemfile
+        end
+      end
+    end
+  end
+
   let(:test_dir) { File.join(File.dirname(__FILE__), "files/test_project") }
 
   before do
-    Bundler.with_clean_env do
-      Dir.chdir test_dir do
-        begin
-          sh "bundle install > #{logfile} 2>&1"
-          sh "bundle exec rake compile >> #{logfile} 2>&1"
-        rescue RuntimeError => e
-          puts "SETUP FAILED:\n#{File.read(logfile)}"
-          raise
-        end
+    do_in_test_dir do
+      begin
+        sh "bundle install > #{logfile} 2>&1"
+        sh "bundle exec rake compile >> #{logfile} 2>&1"
+      rescue RuntimeError => e
+        puts "SETUP FAILED:\n#{File.read(logfile)}"
+        raise
       end
     end
   end
@@ -24,12 +36,10 @@ describe "exit code" do
   context "from a good run" do
     let(:logfile) { "good-run.log" }
     it "is zero" do
-      Bundler.with_clean_env do
-        Dir.chdir test_dir do
-          system "bundle exec rake test:valgrind TESTOPTS='--name /notexist/' >> #{logfile} 2>&1"
-          exitcode = $?
-          expect(exitcode.success?).to(be_truthy, File.read(logfile))
-        end
+      do_in_test_dir do
+        system "bundle exec rake test:valgrind TESTOPTS='--name /notexist/' >> #{logfile} 2>&1"
+        exitcode = $?
+        expect(exitcode.success?).to(be_truthy, File.read(logfile))
       end
     end
   end
@@ -37,12 +47,10 @@ describe "exit code" do
   context "from a bad run" do
     let(:logfile) { "bad-run.log" }
     it "is nonzero" do
-      Bundler.with_clean_env do
-        Dir.chdir test_dir do
-          system "bundle exec rake test:valgrind >> #{logfile} 2>&1"
-          exitcode = $?
-          expect(exitcode.success?).to(be_falsey, File.read(logfile))
-        end
+      do_in_test_dir do
+        system "bundle exec rake test:valgrind >> #{logfile} 2>&1"
+        exitcode = $?
+        expect(exitcode.success?).to(be_falsey, File.read(logfile))
       end
     end
   end
